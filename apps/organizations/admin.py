e94ca63247fe1dfbd63bb2974c3cc9d913bd15b1
hexiaoyuchina <1642567075@qq.com>
2020-04-22 20:42:27 +0800

    admin搭建后台管理

--- apps/organizations/admin.py
+++ apps/organizations/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from apps.organizations.models import City,Teacher,CourseOrg
 # Register your models here.
+
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+#用户信息通过这个绑定，admin创建的用户的密码才能使用刚登陆
+from django.contrib.auth.admin import UserAdmin
 
+from apps.users.models import UserProfile
 # Register your models here.
+class UserProFileAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(UserProfile,UserAdmin)
